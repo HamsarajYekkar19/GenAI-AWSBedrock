fc29382c03eb82ea6ea5c7addf9586528ae0661b
HamsarajYekkar <Hamsarajyekkar19@gmail.com>
2024-07-25 20:42:46 +0530

    Add Image-Generation-Using-Titan and Update gitignore

---
+++ Image-Generation-Using-Titan/titan.py
@@ -0,0 +1,62 @@
+# Use the native inference API to create an image with Titan Image Generator G1
+
+import base64
+import boto3
+import json
+import os
+
+# Create a Bedrock Runtime client in the AWS Region of your choice.
+client = boto3.client("bedrock-runtime", region_name="ap-south-1")
+
+# Set the model ID, e.g., Titan Image Generator G1.
+model_id = "amazon.titan-image-generator-v1"
+
+# Define the image generation prompt for the model.
+prompt = "Indian coastal house in rainy season"
+
+# Format the request payload using the model's native structure.
+native_request = {
+    "textToImageParams":{
+        "text":prompt
+    },
+    "taskType":"TEXT_IMAGE",
+    "imageGenerationConfig":{
+        "cfgScale":8,
+        "seed":0,
+        "width":1024,
+        "height":1024,
+        "numberOfImages":3
+    }
+}
+
+# Convert the native request to JSON.
+request = json.dumps(native_request)
+
+# Invoke the model with the request.
+response = client.invoke_model(modelId=model_id, body=request)
+
+# Decode the response body.
+model_response = json.loads(response["body"].read())
+
+# Extract the image data.
+base64_image_data = model_response["images"][0]
+
+# Save the generated image to a local folder.
+i, output_dir = 1, "Generated_Images"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+while os.path.exists(os.path.join(output_dir, f"image_{i}.png")):
+    i += 1
+
+image_data = base64.b64decode(base64_image_data)
+
+image_path = os.path.join(output_dir, f"image_{i}.png")
+with open(image_path, "wb") as file:
+    file.write(image_data)
+
+import sys 
+
+print(f"The generated image has been saved to {image_path}.")
+print(f"Python executable: {sys.executable}")
+print(f"Environment location: {sys.prefix}")
+print(f"Environment variable: {os.getenv('VIRTUAL_ENV')}")
